0071b2e7c19476a571e54f2bed8697c77476b085
Noelia-Abril <noeliaabrilc@gmail.com>
2019-11-27 19:59:10 -0400

    Actualizacion Base de Datos

--- BdDPizzeria/dbpizzeria.sql
+++ BdDPizzeria/dbpizzeria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-11-2019 a las 19:09:35
+-- Tiempo de generación: 27-11-2019 a las 20:47:06
 -- Versión del servidor: 10.4.8-MariaDB
 -- Versión de PHP: 7.3.11
 
@@ -34,6 +34,22 @@ CREATE TABLE `cliente` (
   `Nombre` varchar(100) COLLATE utf8mb4_spanish2_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`IdCliente`, `CI`, `Nombre`) VALUES
+(2, '0', 'Juan David Martinez Campos'),
+(3, '8948431', 'Guadalupe Escobar Jimenez '),
+(4, '1256197', 'Carmen Julia Gutierrez Illanes'),
+(5, '7569896', 'Dennis Hidalgo Jaimes'),
+(6, '4536127', 'Karen Pacheco Sarmiento'),
+(7, '7895643', 'Sandro Troncoso Kaisler'),
+(8, '7269548', 'Fernanda  Hilda Suxo Fernandez'),
+(9, '1651987', 'Camila Perez'),
+(10, '4863021', 'Pepino Cortez'),
+(11, '0', 'fgxddtdhgfxhfhg');
+
 -- --------------------------------------------------------
 
 --
@@ -42,12 +58,35 @@ CREATE TABLE `cliente` (
 
 CREATE TABLE `detalle` (
   `IdDetalle` int(11) NOT NULL,
-  `Total` decimal(11,2) DEFAULT NULL,
+  `Descuento` double(3,2) NOT NULL,
+  `SubTotal` decimal(11,2) DEFAULT NULL,
   `Pedido_NumPedido` int(11) NOT NULL,
   `Cliente_IdCliente` int(11) NOT NULL,
   `Pizza_Cod_Pz` varchar(15) COLLATE utf8mb4_spanish2_ci NOT NULL,
   `Ingrediente_idIngrediente` varchar(12) COLLATE utf8mb4_spanish2_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleado`
+--
+
+CREATE TABLE `empleado` (
+  `cod_empleado` int(11) NOT NULL,
+  `CI` varchar(10) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NOT NULL,
+  `Nombre` varchar(100) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NOT NULL,
+  `Cargo` varchar(100) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NOT NULL,
+  `id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `empleado`
+--
+
+INSERT INTO `empleado` (`cod_empleado`, `CI`, `Nombre`, `Cargo`, `id`) VALUES
+(2, '7896042', 'Carlos Enrique Bustamante Aranibar', 'Cocinero Turno tarde ', 3),
+(3, '7845692', 'Daniel Torrez Avellana', 'Cajero', 3);
 
 -- --------------------------------------------------------
 
@@ -69,9 +108,13 @@ CREATE TABLE `ingrediente` (
 --
 
 INSERT INTO `ingrediente` (`idIngrediente`, `Nombre`, `Imagen`, `Cantidad`, `Precio`, `Pizza_Cod_Pz`) VALUES
-('464', 'pimienta', '5.jpg', 64, 1.5, '168'),
-('78', 'bacon', NULL, 0, 7, '168'),
-('849', 'cebolla', '10.jpg', 40, 3, '168');
+('1564', 'Jamon Serrano', '34.jpg', 0, 22.6, '5454'),
+('225', 'Queso Mozzarella', '1.jpg', 500, 8, '5498'),
+('363', 'Pimientos', '2.jpg', 60, 2, '6899'),
+('464', 'Oregano', '5.jpg', 100, 0.5, '1245'),
+('761', 'Pepperoni', '28(1).jpg', 200, 6, '1245'),
+('849', 'cebolla', '10.jpg', 40, 3, '168'),
+('919', 'Tocino', '7.jpg', 60, 6, '2258');
 
 -- --------------------------------------------------------
 
@@ -122,7 +165,7 @@ CREATE TABLE `pedido` (
   `NumPedido` int(11) NOT NULL,
   `Tam_Pizza` varchar(20) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NOT NULL,
   `Fecha` date DEFAULT NULL,
-  `Subtotal` decimal(11,2) DEFAULT NULL,
+  `Total` decimal(11,2) DEFAULT NULL,
   `Cliente_IdCliente` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
@@ -147,7 +190,13 @@ CREATE TABLE `pizza` (
 --
 
 INSERT INTO `pizza` (`Cod_Pz`, `PNombre`, `Precio`, `Descripcion`, `Existencias`, `Imagen`, `estado`) VALUES
-('168', 'Pizza suprema', '70.00', 'pizza de queso cebolla y oregano', 70, 'super-suprema.jpg', 'Activo');
+('1245', 'Pizza Pepperoni Lovers', '75.00', 'Salsa de Tomate, Queso 100% Mozzarella, Doble Pepperoni, Extra Mozzarella y Orégano', 17, 'pepperoni-lovers.jpg', 'Activo'),
+('1564', 'Pizza Caribeña', '75.50', 'Oregano queso ', 80, 'caribena.jpg', 'Inactivo'),
+('168', 'Pizza suprema', '70.00', 'Salsa de Tomate, Queso 100% Mozzarella, Ternera, Pepperoni, Cebolla Roja, Champiñones, Pimiento y Orégano', 20, 'suprema.jpg', 'Activo'),
+('2258', 'Pizza Bacon Lovers', '70.80', 'Salsa de Tomate, Queso 100% Mozzarella, Doble Bacon, Extra Mozzarella y Orégano', 20, 'bacon-lovers.jpg', 'Activo'),
+('5454', 'Pizza Clásica', '68.50', 'Salsa de Tomate, Queso 100% Mozzarella, Jamón, Bacon, Champiñones, Extra Mozzarella  y Orégano', 18, 'clasica.jpg', 'Activo'),
+('5498', 'Pizza Cheeseham', '75.50', 'Salsa de Tomate, Queso 100% Mozzarella, Doble Jamón, Extra Mozzarella y Orégano', 15, 'cheeseham.jpg', 'Activo'),
+('6899', 'Pizza Veggie Lovers', '60.00', 'Salsa de Tomate, Queso 100% Mozzarella, Cebolla Roja, Maíz, Mezcla de Pimientos, Champiñones Frescos, Tomate Natural y Orégano', 25, 'veggie-lovers.jpg', 'Activo');
 
 -- --------------------------------------------------------
 
@@ -170,7 +219,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(2, 'Divino Sapori', 'pzdivinosapori@gmail.com', '$2y$10$IPpeujQk4IiQ1Kkjfpf09ee8kVxXvb/n7.WT8TnD.IspGUvMeawXa', '7NVvB21jyaBe7uJZuKwSvkfkuI1aakaTOjqprEyvb98HCDvuCmfEkqlm4ZWo', '2019-11-12 02:20:21', '2019-11-12 02:58:18');
+(2, 'Divino Sapori', 'pzdivinosapori@gmail.com', '$2y$10$IPpeujQk4IiQ1Kkjfpf09ee8kVxXvb/n7.WT8TnD.IspGUvMeawXa', 'DqUD7U8zVjnwFER8EbHNnThrKgjyGN0xtAUdd8EwzcKaPY0DfTUxjmzRCzRF', '2019-11-12 02:20:21', '2019-11-24 04:54:13'),
+(3, 'Empleado Pizzeria', 'EmpleadoDS@hotmail.com', '$2y$10$T695UIDl7.sOAOzvR6912eRWEXmERtZTzUYCEP4VmNHu7Fa2CeNUu', 'jqiiZT6g6QbApbI6Hni4o9BJVNB3wJ9lu1HZ7CvBcOADpvQZNVBl5RBSTdaR', '2019-11-21 03:20:17', '2019-11-24 20:24:46');
 
 --
 -- Índices para tablas volcadas
@@ -191,6 +241,13 @@ ALTER TABLE `detalle`
   ADD KEY `fk_Detalle_Cliente1` (`Cliente_IdCliente`),
   ADD KEY `fk_Detalle_Pizza1` (`Pizza_Cod_Pz`),
   ADD KEY `fk_Detalle_Ingrediente1` (`Ingrediente_idIngrediente`);
+
+--
+-- Indices de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD PRIMARY KEY (`cod_empleado`),
+  ADD KEY `id` (`id`);
 
 --
 -- Indices de la tabla `ingrediente`
@@ -234,13 +291,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `IdCliente` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `IdCliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de la tabla `detalle`
 --
 ALTER TABLE `detalle`
   MODIFY `IdDetalle` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  MODIFY `cod_empleado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `pedido`
@@ -252,7 +315,7 @@ ALTER TABLE `pedido`
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Restricciones para tablas volcadas
@@ -266,6 +329,12 @@ ALTER TABLE `detalle`
   ADD CONSTRAINT `fk_Detalle_Ingrediente1` FOREIGN KEY (`Ingrediente_idIngrediente`) REFERENCES `ingrediente` (`idIngrediente`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_Detalle_Pedido1` FOREIGN KEY (`Pedido_NumPedido`) REFERENCES `pedido` (`NumPedido`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_Detalle_Pizza1` FOREIGN KEY (`Pizza_Cod_Pz`) REFERENCES `pizza` (`Cod_Pz`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD CONSTRAINT `empleado_ibfk_1` FOREIGN KEY (`id`) REFERENCES `users` (`id`);
 
 --
 -- Filtros para la tabla `ingrediente`
